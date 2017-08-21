6fe30c796d05f05a08e64327584f6dbcacdae64d
Alex Samuel <alex@alexsamuel.net>
2017-08-20 21:38:18 -0400

    First crack at converting arrays.

--- tbl/io.py
+++ tbl/io.py
@@ -1,10 +1,59 @@
 import csv
 import logging
+import numpy as np
 
-from   .commands import *
+from   .commands import command, CmdResult, CmdError
 from   .model import Model
 
 #-------------------------------------------------------------------------------
+
+def _convert_to_int(arr):
+    """
+    Tries to convert `arr` to an int array.
+    """
+    empty = arr == ""
+    if empty.all():
+        raise ValueError("no non-empty values")
+    return 
+
+
+def _convert_to_float(arr):
+    """
+    Tries to convert `arr` to a float array, interpreting empty strings as NaN.
+    """
+    empty = arr == ""
+    if empty.all():
+        raise ValueError("no non-empty values")
+
+    val         = np.empty(arr.shape, dtype=float)
+    val[~empty] = arr[~empty].astype(float)
+    val[ empty] = np.nan
+    return val
+
+
+def guess_convert(arr):
+    """
+    Attempt to convert `arr` to "more specific" types.
+    """
+    arr = np.asarray(arr)
+
+    try:
+        return arr.astype(int)
+    except ValueError:
+        pass
+
+    try:
+        return _convert_to_float(arr)
+    except ValueError:
+        pass
+
+    try:
+        return arr.astype(bool)
+    except ValueError:
+        pass
+
+    return arr
+
 
 def load_test(path):
     with open(path) as file:
@@ -14,6 +63,7 @@ def load_test(path):
         arrs = zip(*list(rows))
     mdl = Model(path)
     for arr, name in zip(arrs, names):
+        arr = guess_convert(arr)
         mdl.add_col(arr, name)
     return mdl
 
